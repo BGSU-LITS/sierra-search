4b826fbf0b9250bde03d00443b8c2049064b97a4
John Kloor <john@kloor.org>
2023-12-19 10:58:44 -0500

    PHP 8.2, Framework 2.0

--- src/Lits/Action/IndexAction.php
+++ src/Lits/Action/IndexAction.php
@@ -6,28 +6,56 @@ namespace Lits\Action;
 
 use Lits\Action;
 use Lits\Config\SearchConfig;
+use Lits\Exception\InvalidConfigException;
+use Slim\Exception\HttpInternalServerErrorException;
 use Slim\Exception\HttpNotFoundException;
 
 final class IndexAction extends Action
 {
+    /**
+     * @throws HttpInternalServerErrorException
+     * @throws HttpNotFoundException
+     */
     public function action(): void
     {
         \assert($this->settings['search'] instanceof SearchConfig);
 
-        $this->settings['search']->testCatalogs();
+        try {
+            $this->settings['search']->testCatalogs();
+        } catch (InvalidConfigException $exception) {
+            throw new HttpInternalServerErrorException(
+                $this->request,
+                null,
+                $exception,
+            );
+        }
+
         $catalogs = $this->settings['search']->catalogs;
 
-        if (!isset($this->data['catalog'])) {
-            $this->render($this->template(), ['catalogs' => $catalogs]);
+        if (isset($this->data['catalog'])) {
+            if (!isset($catalogs[$this->data['catalog']])) {
+                throw new HttpNotFoundException($this->request);
+            }
+
+            $this->catalog($catalogs[$this->data['catalog']]);
 
             return;
         }
 
-        if (!isset($catalogs[$this->data['catalog']])) {
-            throw new HttpNotFoundException($this->request);
+        try {
+            $this->render($this->template(), ['catalogs' => $catalogs]);
+        } catch (\Throwable $exception) {
+            throw new HttpInternalServerErrorException(
+                $this->request,
+                null,
+                $exception,
+            );
         }
+    }
 
-        $url = \rtrim($catalogs[$this->data['catalog']], '/') . '/';
+    public function catalog(string $catalog): void
+    {
+        $url = \rtrim($catalog, '/') . '/';
 
         if (isset($this->data['function'])) {
             $url .= $this->data['function'];
--- src/Lits/Config/SearchConfig.php
+++ src/Lits/Config/SearchConfig.php
@@ -6,37 +6,52 @@ namespace Lits\Config;
 
 use Lits\Config;
 use Lits\Exception\InvalidConfigException;
+use Safe\Exceptions\PcreException;
 
 use function Safe\preg_match;
-use function Safe\sprintf;
 
 final class SearchConfig extends Config
 {
     /** @var array<string, string> */
     public array $catalogs = [];
 
+    /** @throws InvalidConfigException */
     public function testCatalogs(): void
     {
         if (\count($this->catalogs) === 0) {
             throw new InvalidConfigException(
-                'At least one catalog must be specified'
+                'At least one catalog must be specified',
             );
         }
 
         foreach ($this->catalogs as $slug => $url) {
-            if (\filter_var($url, \FILTER_VALIDATE_URL) === false) {
-                throw new InvalidConfigException(sprintf(
-                    'A valid URL is required for catalog %s.',
-                    $slug
-                ));
-            }
+            $this->testCatalogsUrl($slug, $url);
+        }
+    }
 
+    /** @throws InvalidConfigException */
+    private function testCatalogsUrl(string $slug, string $url): void
+    {
+        if (\filter_var($url, \FILTER_VALIDATE_URL) === false) {
+            throw new InvalidConfigException(\sprintf(
+                'A valid URL is required for catalog %s.',
+                $slug,
+            ));
+        }
+
+        try {
             if (preg_match('/^https?:/i', $url) === 0) {
-                throw new InvalidConfigException(sprintf(
+                throw new InvalidConfigException(\sprintf(
                     'A HTTP(S) URL is required for catalog %s.',
-                    $slug
+                    $slug,
                 ));
             }
+        } catch (PcreException $exception) {
+            throw new InvalidConfigException(
+                \sprintf('Could not validate URL for catalog %s.', $slug),
+                0,
+                $exception,
+            );
         }
     }
 }
